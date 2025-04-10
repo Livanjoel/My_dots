1e36f65fadf049806be02838e9444db3822de030
Livanjoel <livanjoel777@gmail.com>
2025-04-10 14:36:56 -0400

    deleted because wrong organization

--- lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set("n", "ws", "<cmd>w!<CR>")
-vim.keymap.set("n", "wq", "<cmd>wqa!<CR>")
-vim.keymap.set("n", "qq", "<cmd>qa!<CR>")
-vim.keymap.set("n", "<c-Left>", "<cmd>BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<c-Right>", "<cmd>BufferLineCycleNext<CR>")
